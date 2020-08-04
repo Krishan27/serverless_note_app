f944da30528bbd22a1ed2a23db0cc82d2d2192ad
Krishan27 <ks2616459@gmail.com>
2020-08-04 01:30:49 -0500

    changes

--- handler.js
+++ handler.js
@@ -1,25 +1,6 @@
-"use strict";
-const { tagEvent } = require("./serverless_sdk");
+'use strict';
 
-module.exports.hello = async event => {
-  tagEvent("custom-tag", "hello world", { custom: { tag: "data" } });
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.hello = (event, context, callback) => {
+	console.log('Hello World');
+	callback(null, 'Hello World');
 };
